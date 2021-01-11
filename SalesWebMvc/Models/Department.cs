92e37da622a79a8840ccd10d556297f274c0a789
iGsrc <ivan.galvagno@gmail.com>
2021-01-11 07:11:49 -0300

    Added the domains models, classes and associations with their respective properties and methods

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,8 +1,37 @@
-﻿namespace SalesWebMvc.Models
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        //Association
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        //Constructors
+        public Department() { }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        //methods
+       public void AddSeler(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+       public void RemoveSeller(Seller seller)
+        {
+            Sellers.Remove(seller);
+        }
+        public double TotalSeler(DateTime inital, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(inital, final));   
+        }
     }
 }
---
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SalesWebMvc.Models.Enums;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SalesStatus Status { get; set; }
+
+        //Association
+        public Seller Seller { get; set; }
+
+        //Constructor
+        public SalesRecord() { }
+
+        public SalesRecord(int id, DateTime date, double amount, SalesStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
